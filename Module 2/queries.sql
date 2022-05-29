4851317e7fb4112db9aa654ebe832676f8a6c361
Vainane <91353205+Vainane@users.noreply.github.com>
2022-05-29 08:30:41 +0300

    Update queries.sql

--- Module 2/queries.sql
+++ Module 2/queries.sql
@@ -1,53 +1,107 @@
--- 1.Total sales AND total profit.
-SELECT 
-	Order_Date,
-	ROUND(SUM(o.sales), 2) AS total_sales,
-	ROUND(SUM(o.profit), 2) AS total_profit
-FROM 
-	orders AS o 
-GROUP BY 
-	Order_Date
+/*Total Sales per year.
+  Total Profit per year.
+  Total Ratio per year.
+  Average Discount per year.*/
+  SELECT 
+         EXTRACT(YEAR FROM o.order_date) AS year,
+	     ROUND(SUM(o.sales), 2) AS total_sales,
+	     ROUND(SUM(o.profit), 2) AS total_profit,
+    	 ROUND(SUM(o.profit) / SUM(o.sales) * 100) AS profit_ratio_percentage,  -- Profit ratio = (net profit / net sales) x 100
+	     ROUND(AVG(o.discount) * 100) AS average_discount_percentage
+    FROM 
+         superstore.orders AS o 
+GROUP BY
+         year
 ORDER BY 
-	Order_Date;
+         year;
 
--- Profit per Order
-SELECT
-	Order_ID,
-	round(sum(sales), 2) AS sales,
-	round(sum(profit), 2) AS profit
-FROM
-	orders
+-- Profit per Order.
+  SELECT
+         o.order_id AS number_order, 
+		 ROUND(SUM(o.profit), 2) AS profit_per_order  
+    FROM 
+         superstore.orders AS o
 GROUP BY
-	Order_ID;
-
--- 2. Sales by Product Category over time
-SELECT
-	category,
-	ROUND(SUM(sales),2) AS sales,
-	ROUND(SUM(profit),2) AS profit
-FROM
-	orders
-GROUP BY
-	category;
-
--- 3.Sales and Profit by Customer
-SELECT 
-	customer_name,
-	ROUND(SUM(sales), 2) AS total_sales,
-	ROUND(SUM(profit), 2) AS total_profit
-FROM
-	orders
-GROUP BY
- 	customer_name;
-
--- Sales per region
-SELECT 
-	region,
-	ROUND(SUM(sales), 2) AS total_sales
-FROM 
-	orders 
-GROUP BY 
-	region
+         o.order_id
 ORDER BY 
-	region;
+         o.order_id;
+         
+-- Sales per Customer.
+  SELECT 
+         o.customer_name, 
+		 ROUND(SUM(o.sales), 2) AS total_sales
+    FROM 
+         superstore.orders AS o
+GROUP BY 
+         o.customer_name
+ORDER BY
+         o.customer_name;
+         
+-- Monthly Sales by Segment.
+  SELECT
+         date_trunc('month', o.order_date) AS month,
+		 ROUND(SUM(o.sales), 2) AS sales_in_segment,
+         o.segment
+    FROM 
+         superstore.orders AS o 
+GROUP BY 
+         month,
+         segment
+ORDER BY 
+         month,
+         segment;
 
+-- Monthly Sales by product category.
+  SELECT 
+         date_trunc('month', o.order_date) AS month,
+	   	 o.category AS product_category,
+		 ROUND(SUM(o.sales), 2) AS total_sales
+    FROM  
+         superstore.orders AS o 
+GROUP BY  
+         month, o.category
+ORDER BY 
+         month, o.category;
+
+-- Sales by Product Category over time.
+  SELECT
+         ROUND(SUM(sales), 2) AS total_sales,
+         o.category 
+    FROM 
+         superstore.orders AS o 
+GROUP BY
+         category;
+         
+-- Sales and Profit by Customer.
+  SELECT 
+         o.customer_name, 
+		 ROUND(SUM(o.sales), 2) AS sales_per_customer,  
+		 round(SUM(o.profit), 2) AS profit_per_customer  
+    FROM  
+         superstore.orders AS o
+GROUP BY
+         o.customer_name
+ORDER BY 
+         o.customer_name;
+
+-- Customer ranking.
+  SELECT 
+         o.customer_name, 
+		 ROUND(SUM(o.profit), 2) AS profit_per_customer
+    FROM 
+         superstore.orders AS o
+GROUP BY 
+         o.customer_name
+ORDER BY 
+         profit_per_customer DESC;
+         
+-- Sales per Region.
+  SELECT 
+         o.region, 
+		 ROUND(SUM(o.sales), 2) AS total_sales
+    FROM 
+         superstore.orders AS o
+GROUP BY 
+         o.region
+ORDER BY
+         o.region;
